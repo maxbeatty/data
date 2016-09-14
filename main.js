563b23daa16b7780a9e547be2d09f1cc9a7e86d6
robmadden <rob.madden@bloc.io>
2016-09-14 11:14:39 -0700

    Add lint checks

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-"use strict";
+const winston = require('winston');
+
+// Set log level to debug
+winston.level = 'debug';
 
 // If the environgment does not have the proper env variables set, exit early.
 // SLACK_RTM_TOKEN: Slack Real Time Token
 // PROJECT_ROOT: The root directory of the project
 if (!process.env.SLACK_RTM_TOKEN || !process.env.PROJECT_ROOT) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  winston.log('debug', 'Error: Specify token in environment');
+  process.exit(1);
 }
 
 require('./scripts/characters');
--- scripts/characters.js
+++ scripts/characters.js
@@ -1,54 +1,68 @@
-var Botkit = require('botkit');
-var cheerio = require('cheerio'), cheerioTableparser = require('cheerio-tableparser');
+const Botkit = require('botkit');
+const cheerio = require('cheerio');
+const cheerioTableparser = require('cheerio-tableparser');
 const fs = require('fs');
-const CHARACTERS_FILE = process.env.PROJECT_ROOT + 'static_data/characters.html';
-//console.log(CHARACTERS_FILE);
+
+const MENTION_HEARS_PATTERN = '(.*)character(.*)';
+const MENTION_TYPES = ['direct_message', 'direct_mention', 'mention'];
+const CHARACTERS_FILE = `${process.env.PROJECT_ROOT}/static_data/characters.html`;
+const CONTROLLER = Botkit.slackbot({
+  debug: false,
+  // include 'log: false' to disable logging
+  // or a 'logLevel' integer from 0 to 7 to adjust logging verbosity
+});
+
+let characters = null;
+let characterCount = 0;
+
+// const winston = require('winston');
+// winston.log('debug', CHARACTERS_FILE);
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+}
 
-// Parse characters.html file to get list of characters
-var characterCount = 0;
+function readFileHandler(err, contents) {
+  const $ = cheerio.load(contents);
+  cheerioTableparser($);
+  characters = $('#characters').parsetable(true, true, true);
+  characterCount = characters[0].length;
+  // winston.log('debug', 'Parsed ' + character_count + ' characters.');
+}
 
 // Open characters.html
-fs.readFile(CHARACTERS_FILE, 'utf8', function(err, contents) {
-  $ = cheerio.load(contents);
-  cheerioTableparser($);
-  characters = $("#characters").parsetable(true, true, true);
-  characterCount = characters[0].length;
-  // console.log("Parsed " + character_count + " characters.");
-});
+function readFile() {
+  fs.readFile(CHARACTERS_FILE, 'utf8', readFileHandler);
+}
 
-var controller = Botkit.slackbot({
-  debug: false
-  //include "log: false" to disable logging
-  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
-});
+// Callback used by CONTROLLER.hears
+function controllerHandler(bot, message) {
+  // Default character is Data
+  let character = 'Data';
+  const randomIndex = getRandomInt(0, characterCount);
+  const name = characters[0][randomIndex];
+  const rank = characters[4][randomIndex] == null
+    ? 'n/a'
+    : characters[4][randomIndex];
+  const position = characters[5][randomIndex] == null
+    ? 'n/a'
+    : characters[5][randomIndex];
+  const species = characters[6][randomIndex];
+  character = `*Name:* ${name}\n
+    *Rank*: ${rank}\n
+    *Position*: ${position}\n
+    *Species*: ${species}\n`;
+  bot.reply(message, character);
+}
+
+readFile();
 
 // connect the bot to a stream of messages
-controller.spawn({
+CONTROLLER.spawn({
   token: process.env.SLACK_RTM_TOKEN,
-}).startRTM()
+}).startRTM();
 
 // give the bot something to listen for.
-controller.hears('(.*)character(.*)',['direct_message','direct_mention','mention'], function(bot,message) {
-  // Default character is Data
-  var character = "Data";
-  randomCharacterIndex = getRandomInt(0, characterCount);
-  var name = characters[0][randomCharacterIndex];
-  var rank = characters[4][randomCharacterIndex] == null
-    ? "n/a"
-    : characters[4][randomCharacterIndex];
-  var position = characters[5][randomCharacterIndex] == null
-    ? "n/a"
-    : characters[5][randomCharacterIndex];
-  var species = characters[6][randomCharacterIndex];
-  character = "*Name:* " + name + "\n" +
-              "*Rank*: " + rank + "\n" +
-              "*Position*: " + position + "\n" +
-              "*Species*: " + species + "\n";
-  bot.reply(message, character);
-});
+CONTROLLER.hears(MENTION_HEARS_PATTERN, MENTION_TYPES, controllerHandler);
